6d81aa07ff1f2bdfc824faa629ebd17e68d4dfad
vteamdell0723 <vteamdell0723@zhangjiajiang>
2014-02-21 13:48:37 +0800

    PyThonProject

---
+++ com/ktproject/reptile/KTProjectXLReptile.py
@@ -0,0 +1,21 @@
+
+import urllib
+import re
+
+def  openUrl(webHttp):
+    urlopen  = urllib.urlopen(webHttp) 
+    context = urlopen.read()
+    return  context
+
+context = openUrl('http://blog.sina.com.cn/s/articlelist_1284139322_0_1.html')
+
+pattern = re.compile(r'<span class="atc_title">  </span>')
+filterContexts = re.findall(pattern, context)
+
+for  filterContext  in filterContexts:
+    print filterContext
+  
+  
+  
+
+    
