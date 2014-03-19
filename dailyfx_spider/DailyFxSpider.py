047835ee2cd5ca17030dad0af704b08d2585f00d
unknown <vteamdell0723@.(none)>
2014-03-19 17:52:42 +0800

    sourceDesign

--- dailyfx_spider/DailyFxSpider.py
+++ dailyfx_spider/DailyFxSpider.py
@@ -5,8 +5,10 @@ import DailyFxSpiderUtils
 if __name__ =='__main__':
     
     startContext = DailyFxSpiderUtils.retrunStartContext('http://cdn.dailyfx.com.hk/livenews/index.html')
-    print DailyFxSpiderUtils.divisionTarget(startContext,'<tr class="record" valign="top">', '</tr>')
-    print len(DailyFxSpiderUtils.findAllTarget(startContext))
+    dict = DailyFxSpiderUtils.divisionTarget(startContext,'<tr class="record" valign="top">', '</tr>')
+    length = len(DailyFxSpiderUtils.findAllTarget(startContext))
+    print dict['targetContext']
+    print dict['nextContext']
 
 
 
