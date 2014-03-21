57e237069e8d352d980306f727a3581b85c8946e
unknown <vteamdell0723@.(none)>
2014-03-21 17:58:05 +0800

    sourceDesign

--- cnforex_spider/CnforexSpider.py
+++ cnforex_spider/CnforexSpider.py
@@ -2,9 +2,8 @@
 import CnforexSpiderUtils
 
 
-
-
 def findForexImage():
+    defaultLink = 'http://www.cnforex.com/news/tuce/'
     startcontext = CnforexSpiderUtils.returnStartContext('http://www.cnforex.com/news/tuce/')
     targetLen = CnforexSpiderUtils.findAllTarget(startcontext)
     for i in range(targetLen):
@@ -14,7 +13,9 @@ def findForexImage():
         title = CnforexSpiderUtils.filterContextByTarget(currentcontext, '<p>\r\n                        ', '</p>')
         imageurl = CnforexSpiderUtils.filterContextByTarget(currentcontext, '<img src="', '" />')
         currentlink = CnforexSpiderUtils.filterContextByTarget(currentcontext,'<a href="','">\r\n                        <img')
-        print {'title':title,'imageurl':imageurl,'link':currentlink}
+        print {'title':title,'imageurl':imageurl,'link':defaultLink+currentlink}
+        
+        
 if __name__ == '__main__':
     findForexImage()
     
