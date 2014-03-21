29f5d42d2813c678395e46ddeba46a74bce720c4
unknown <vteamdell0723@.(none)>
2014-03-21 15:32:35 +0800

    sourceDesign

--- cnforex_spider/CnforexSpider.py
+++ cnforex_spider/CnforexSpider.py
@@ -1,3 +1,21 @@
 
+import CnforexSpiderUtils
 
 
+
+
+def findForexImage():
+    startcontext = CnforexSpiderUtils.returnStartContext('http://www.cnforex.com/news/tuce/')
+    targetLen = CnforexSpiderUtils.findAllTarget(startcontext)
+    for i in range(targetLen):
+        targetContext = CnforexSpiderUtils.divisionTarget(startcontext, '<div class="imgModel">', '</div>')
+        startcontext = targetContext['nextContext']
+        currentcontext =  targetContext['targetContext']
+        title = CnforexSpiderUtils.filterContextByTarget(currentcontext, '<p>\r\n                        ', '</p>')
+        imageurl = CnforexSpiderUtils.filterContextByTarget(currentcontext, '<img src="', '" />')
+        currentlink = CnforexSpiderUtils.filterContextByTarget(currentcontext,'<a href="','">\r\n                        <img')
+        print {'title':title,'imageurl':imageurl,'link':currentlink}
+if __name__ == '__main__':
+    findForexImage()
+    
+    
--- cnforex_spider/CnforexSpiderUtils.py
+++ cnforex_spider/CnforexSpiderUtils.py
@@ -1,9 +1,24 @@
 import sys
 sys.path.append("../commonutils_spider/")
-
+import CommonsSpiderUtils
 
 class  CnforexSpiderUtils:
     def __init__(self):
         self.server = 'localhost'
 
+def  returnStartContext(link):
+    context = CommonsSpiderUtils.openUrl(link)
+    startContext = CommonsSpiderUtils.startContext(context,'<div class="imgModel">')
+    return startContext
 
+def findAllTarget(context):
+    return len(CommonsSpiderUtils.findAllTarget(context,r'<div class="imgModel">'))  
+
+
+def divisionTarget(startcontext,startfilter,endfilter):
+    startIndex = CommonsSpiderUtils.filterContext(startcontext,startfilter)
+    endIndex = CommonsSpiderUtils.filterContext(startcontext,endfilter)+len(endfilter)
+    return {'targetContext':startcontext[startIndex:endIndex],'nextContext':startcontext[endIndex:]}
+
+def filterContextByTarget(context,startfilter,endfilter):
+    return context[CommonsSpiderUtils.filterContext(context,startfilter)+len(startfilter):CommonsSpiderUtils.filterContext(context,endfilter)]
