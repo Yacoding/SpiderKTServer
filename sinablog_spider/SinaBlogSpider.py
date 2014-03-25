f8deb80e6faa036482cb832424d6938c94342766
unknown <vteamdell0723@.(none)>
2014-03-25 10:39:10 +0800

    sourceEditor

--- sinablog_spider/SinaBlogSpider.py
+++ sinablog_spider/SinaBlogSpider.py
@@ -4,6 +4,7 @@ import SinaBlogSpiderUtils
 def dailySinaBlog():
     link ='http://blog.sina.com.cn/s/articlelist_1092672395_0_1.html'
     startcontext = SinaBlogSpiderUtils.returnStartContext(link)
+    blogList = []
     for  i in range(SinaBlogSpiderUtils.findAllTarget(startcontext)):
         targetContext = SinaBlogSpiderUtils.divisionTarget(startcontext, '<div class="articleCell SG_j_linedot1">', '</div>')
         startcontext = targetContext['nextContext']
@@ -13,7 +14,9 @@ def dailySinaBlog():
         linkUrl = SinaBlogSpiderUtils.filterContextByTarget(currentcontext, 'target="_blank" href="', '.html">')+'.html'
         pubDate = SinaBlogSpiderUtils.filtetContextExpertise(currentcontext,'<span class="atc_tm SG_txtc">','</span>')
         imageUrl = SinaBlogSpiderUtils.filtetContextExpertise(currentcontext, 'src="', '" width=')
-        print title+'------'+linkUrl+'----------'+pubDate+'------'+imageUrl
+        blogList.append({'title':title,'linkUrl':linkUrl,'pubDate':pubDate,'imageUrl':imageUrl})
+    return blogList
+
 
 if __name__ =='__main__':
     dailySinaBlog()
