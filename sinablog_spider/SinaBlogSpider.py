127059987fc3d5dc42c7ec4206b78e4966998dc9
unknown <vteamdell0723@.(none)>
2014-03-25 10:34:05 +0800

    sourceEditor

--- sinablog_spider/SinaBlogSpider.py
+++ sinablog_spider/SinaBlogSpider.py
@@ -1,1 +1,19 @@
+import SinaBlogSpiderUtils
 
+
+def dailySinaBlog():
+    link ='http://blog.sina.com.cn/s/articlelist_1092672395_0_1.html'
+    startcontext = SinaBlogSpiderUtils.returnStartContext(link)
+    for  i in range(SinaBlogSpiderUtils.findAllTarget(startcontext)):
+        targetContext = SinaBlogSpiderUtils.divisionTarget(startcontext, '<div class="articleCell SG_j_linedot1">', '</div>')
+        startcontext = targetContext['nextContext']
+        currentcontext =  targetContext['targetContext']
+        print currentcontext
+        title = SinaBlogSpiderUtils.filterContextByTarget(currentcontext, '<a title="', '" target=')
+        linkUrl = SinaBlogSpiderUtils.filterContextByTarget(currentcontext, 'target="_blank" href="', '.html">')+'.html'
+        pubDate = SinaBlogSpiderUtils.filtetContextExpertise(currentcontext,'<span class="atc_tm SG_txtc">','</span>')
+        imageUrl = SinaBlogSpiderUtils.filtetContextExpertise(currentcontext, 'src="', '" width=')
+        print title+'------'+linkUrl+'----------'+pubDate+'------'+imageUrl
+
+if __name__ =='__main__':
+    dailySinaBlog()
--- sinablog_spider/SinaBlogSpiderUtils.py
+++ sinablog_spider/SinaBlogSpiderUtils.py
@@ -1,1 +1,24 @@
+import sys
+sys.path.append("../commonutils_spider/")
+import CommonsSpiderUtils
 
+def returnStartContext(link):
+    currentContext = CommonsSpiderUtils.openUrl(link)
+    startContext = CommonsSpiderUtils.startContext(currentContext,'<div class="articleCell SG_j_linedot1">')
+    return startContext
+
+def findAllTarget(context):
+    return len(CommonsSpiderUtils.findAllTarget(context,r'<div class="articleCell SG_j_linedot1">')) 
+
+def divisionTarget(startcontext,startfilter,endfilter):
+    startIndex = CommonsSpiderUtils.filterContext(startcontext,startfilter)
+    endIndex = CommonsSpiderUtils.filterContext(startcontext,endfilter)+len(endfilter)
+    return {'targetContext':startcontext[startIndex:endIndex],'nextContext':startcontext[endIndex:]}
+
+def filterContextByTarget(context,startfilter,endfilter):
+    return context[CommonsSpiderUtils.filterContext(context,startfilter)+len(startfilter):CommonsSpiderUtils.filterContext(context,endfilter)]
+
+def filtetContextExpertise(context,startfilter,endfilter):
+    finterIndex =  CommonsSpiderUtils.filterContext(context,startfilter)
+    filterContext = context[finterIndex:]
+    return filterContextByTarget(filterContext,startfilter,endfilter)
