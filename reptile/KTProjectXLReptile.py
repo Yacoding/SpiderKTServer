15360f5684964bbbad125bbe406716db1095362f
unknown <vteamdell0723@.(none)>
2014-03-26 17:59:52 +0800

    sourceEditor

--- reptile/KTProjectXLReptile.py
+++ reptile/KTProjectXLReptile.py
@@ -1,19 +1,16 @@
+import uuid
 
-import urllib
-import re
- 
-def  openUrl(webHttp):
-    urlopen  = urllib.urlopen(webHttp) 
-    context = urlopen.read()
-    return  context
 
-context = openUrl('http://blog.sina.com.cn/s/articlelist_1284139322_0_1.html')
 
-pattern = re.compile(r'<span class="atc_title">  </span>')
-filterContexts = re.findall(pattern, context)
 
-for  filterContext  in filterContexts:
-    print filterContext
+
+
+
+
+
+if __name__ =='__main__':
+    print uuid.uuid1()
+
   
   
   
