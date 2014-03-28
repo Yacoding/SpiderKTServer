3546d9b2309e0bf1f650438e3ae6b30f6b0b6bdc
unknown <vteamdell0723@.(none)>
2014-03-28 14:08:36 +0800

    sourceEditor

--- reptile_test/KTRedisScript.py
+++ reptile_test/KTRedisScript.py
@@ -21,7 +21,8 @@ def getRedisData(name):
 if __name__ == '__main__':
     conn = getRedisConn('localhost',6379)
     for i in  range(len(conn.keys())):
-        print conn.get(conn.keys()[i])
+        print conn.keys()[i]
+        #print conn.get(conn.keys()[i])
     
     #str change to dirt
     #print dirt
