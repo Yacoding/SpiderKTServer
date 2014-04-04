7782c65318b52887d728cd5985872f99c006b927
wusezhang <wuse_zjj@163.com>
2014-04-04 21:57:19 +0800

    sourceEditor

--- cnforex_spider/CnforexSpiderUtils.py
+++ cnforex_spider/CnforexSpiderUtils.py
@@ -41,7 +41,7 @@ def findAllImageTarget(startContext):
 
 def returnMySQLConn():
     try:
-        conn = MySQLdb.connect(host='localhost',user='root',passwd='4559065',db='ktproject',port=3306,charset='utf8')
+        conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='4559065',db='ktproject',port=3306,charset='utf8')
     except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
     return conn
--- reptile_test/KTMySQLScript.py
+++ reptile_test/KTMySQLScript.py
@@ -6,7 +6,7 @@ import  MySQLdb
 def returnMySQLConn():
     try:
         conn = MySQLdb.connect(host='localhost',user='root',passwd='4559065',db='ktproject',port=3306)
-    except MySQLdb.Error , e:
+    except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
     return conn
 
