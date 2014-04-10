80d96d142f26055555428c973b244bef24843483
unknown <vteamdell0723@.(none)>
2014-04-10 15:54:37 +0800

    sourceEditor

--- reptile_test/KTMySQLScript.py
+++ reptile_test/KTMySQLScript.py
@@ -3,16 +3,17 @@ import  MySQLdb
 
 
 
-def returnMySQLConn():
+def returnMySQLConn(param):
     try:
         conn = MySQLdb.connect(host='localhost',user='root',passwd='4559065',db='ktproject',port=3306)
+        cursor = conn.cursor()
+        count = cursor.execute("SELECT  COUNT(*)  FROM   whkt_resource_table A  WHERE  A.ID =%s"%param)
+        result = count.fetchone()
+        print result
     except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-    return conn
-
-
 
 
 
 if __name__ =='__main__':
-    print returnMySQLConn()
+    returnMySQLConn('sss')
