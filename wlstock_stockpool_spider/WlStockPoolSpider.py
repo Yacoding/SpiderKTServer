c94f7687de564699c2b2c00d975bb0f432818458
unknown <vteamdell0723@.(none)>
2014-04-21 15:32:22 +0800

    sourceEditor

--- wlstock_stockpool_spider/WlStockPoolSpider.py
+++ wlstock_stockpool_spider/WlStockPoolSpider.py
@@ -1,2 +1,45 @@
-import WlStockPoolSpiderUtils;
+import WlStockPoolSpiderUtils
+import uuid
 
+
+def crawlStockPool(link):
+    startContext = WlStockPoolSpiderUtils.returnStartContext(link,'<div class="arrowlist">');
+    startcontext = WlStockPoolSpiderUtils.returnFilterMainContext(startContext,'<div class="arrowlist">', '<div class="clear marb"></div>')
+    for i in range(WlStockPoolSpiderUtils.findAllTarget(startcontext,'<ul>')):
+        targetContext = WlStockPoolSpiderUtils.divisionTarget(startcontext,'<ul>','</ul>')
+        startcontext =  targetContext['nextContext']
+        currentcontext = targetContext['targetContext']
+        stockMain = WlStockPoolSpiderUtils.filterContextByTarget(currentcontext,'<b>','</b></h2>')
+        gpcId = str(uuid.uuid1())
+        #print 'uuid: '+ gpcId +' stockMain: '+stockMain
+        startFilterContext = currentcontext
+        filterCurrentForumSet = []
+        filterStockForum = []
+        ## FILTER CURRENT UL LIST
+        for count in range(WlStockPoolSpiderUtils.findAllTarget(currentcontext, '<li>')):
+            filterTargetContext = WlStockPoolSpiderUtils.divisionTarget(startFilterContext,'<li>','</li>')
+            filterCurrentContext = filterTargetContext['targetContext']
+            startFilterContext = filterTargetContext['nextContext']
+            filterLinkUrl = WlStockPoolSpiderUtils.filterContextByTarget(filterCurrentContext,"<a href='",' target="_blank">')
+            linkUrl = filterLinkUrl[:len(filterLinkUrl)-1]
+            stockSector = WlStockPoolSpiderUtils.filterContextByTarget(filterCurrentContext,'target="_blank">','</a>')
+            stockForumDescription = ''
+            filterStockPoolList(linkUrl)
+            filterCurrentForumSet.append([id,linkUrl,stockSector,stockForumDescription])
+        #print filterCurrentForumSet
+
+
+def filterStockPoolList(link):
+    startContext = WlStockPoolSpiderUtils.returnStartContext(link,'<div class="arrowlist f14px">');
+    stockForumDescription = WlStockPoolSpiderUtils.filterContextByTarget(startContext, '</strong>', '</p>')
+    for index in range(WlStockPoolSpiderUtils.findAllTarget(startContext, '<li>')):
+        targetContext = WlStockPoolSpiderUtils.divisionTarget(startContext,'<li>','</li>')
+        startContext = targetContext['nextContext']
+        currentcontext = targetContext['targetContext']
+        print currentcontext
+        
+
+if __name__=="__main__":
+    link = 'http://www.wlstock.com/GuPiao/StockPool.aspx'
+    crawlStockPool(link)
+    
--- wlstock_stockpool_spider/WlStockPoolSpiderUtils.py
+++ wlstock_stockpool_spider/WlStockPoolSpiderUtils.py
@@ -4,5 +4,29 @@ import CommonsSpiderUtils
 import CommonsMysqlUtils
 
 
+# FIND FINTER  INFORMATION
+def returnStartContext(link,filter):
+    currentContext = CommonsSpiderUtils.openUrl(link)
+    startContext = CommonsSpiderUtils.startContext(currentContext,filter)
+    return startContext
 
+# FILTER MAIN CONTEXT FROM THE 'http://www.wlstock.com/GuPiao/StockPool.aspx'
+def returnFilterMainContext(startcontext,startfilter,endfilter):
+    startIndex = CommonsSpiderUtils.filterContext(startcontext,startfilter)+len(startfilter)
+    endIndex = CommonsSpiderUtils.filterContext(startcontext,endfilter)
+    return  startcontext[startIndex:endIndex]
+
+# FILTER ALL TARGET INFORMATION
+def findAllTarget(context,filter):
+    return len(CommonsSpiderUtils.findAllTarget(context,r'%s'%filter)) 
+
+# FILTER CONTEXT BY TARGET 
+def divisionTarget(startcontext,startfilter,endfilter):
+    startIndex = CommonsSpiderUtils.filterContext(startcontext,startfilter)
+    endIndex = CommonsSpiderUtils.filterContext(startcontext,endfilter)+len(endfilter)
+    return {'targetContext':startcontext[startIndex:endIndex],'nextContext':startcontext[endIndex:]}
+
+# FIND INFORMATION BY TARGET
+def filterContextByTarget(context,startfilter,endfilter):
+    return context[CommonsSpiderUtils.filterContext(context,startfilter)+len(startfilter):CommonsSpiderUtils.filterContext(context,endfilter)]
 
