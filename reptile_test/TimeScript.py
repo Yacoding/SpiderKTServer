695b24a1fbb6f85a6ef779197ac574af70b05c4c
unknown <vteamdell0723@.(none)>
2014-05-14 14:56:51 +0800

    SOURCEEDITOR

--- reptile_test/TimeScript.py
+++ reptile_test/TimeScript.py
@@ -3,10 +3,10 @@ import sys
 import re
 
 sys.path.append("../dailyfx_spider/")
-import DailyFxSpider
+import DailyFxNewsSpider
 
 def task():
-    DailyFxSpider.dailyForexNews()
+    DailyFxNewsSpider.writeDailyForexNews()
     
 def timer(n):
     while True:
@@ -14,19 +14,9 @@ def timer(n):
         task()
         time.sleep(n) 
 
-def formateTime():
-    
-    
-    print "abcd3r12abhsx"
-    print time.strftime('%Y',time.localtime(time.time()))
-    m = re.findall(r'[\d|.]+', "abcd3r12abhsx") 
-    print (m)
-    return 
-    
 
 if __name__ == '__main__':
-    #timer(60)
-    formateTime()
+    timer(120)
     
     
     
