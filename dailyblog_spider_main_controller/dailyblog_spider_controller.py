49d1b2ab48491fb7974b453a26cf86fba3176f5f
unknown <vteamdell0723@.(none)>
2014-05-15 15:12:01 +0800

    SOURCEEDITOR

--- dailyblog_spider_main_controller/dailyblog_spider_controller.py
+++ dailyblog_spider_main_controller/dailyblog_spider_controller.py
@@ -1,6 +1,5 @@
 import  sys
-sys.path.append("../hexunblog_spider/")
-sys.path.append("../sinablog_spider/")
+sys.path.append("../netblog_spider/")
 import HexunBlogSpider
 import SinaBlogSpider
 from distutils import log
@@ -9,9 +8,12 @@ def updateBlogData():
    
     #SINA NET BLOG SPIDER
     log.info('The system crawling the information of  sina blog')
+    print '----SINA NET BLOG SPIDER START---'
     SinaBlogSpider.writeDailySinaBlog()
+    
     #HEXUN NET BLOG SPIDER
     log.info('The system crawling the informarion of  xehun net')
+    print '---HEXUN NET BLOG SPIDER START---'
     HexunBlogSpider.writeHexunBlog()
       
 if __name__ =="__main__":
