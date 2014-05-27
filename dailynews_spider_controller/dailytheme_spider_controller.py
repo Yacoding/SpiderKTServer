eb5b0a12313e0a9d6a257b73997004c531fe015e
unknown <vteamdell0723@.(none)>
2014-05-27 16:22:07 +0800

    SOURCEEDITOR

--- dailynews_spider_controller/dailytheme_spider_controller.py
+++ dailynews_spider_controller/dailytheme_spider_controller.py
@@ -7,15 +7,19 @@ import YiCaiCompanyNewsSpider
 
 def  crawThemeNews():
     # CRAW THE IMPORT NEWS
+    print '----START CRAW THE IMPORT NEWS----'
     ImportantNewsSpider.writeCompanyNews()
     
     #CRAW THE COMPANY NEWS
+    print '----START CRAW THE COMPANY NEWS----'
     CompanyNewsSpider.writeCompanyNews()
     
     #CRAW THE THEME NEWS
+    print '----START CRAW THE THEME NEWS----'
     ThemeNewsSpider.writeThemeDailyNews()
     
     #CRAW THE YICAI NEWS
+    print '----START CRAW THE YICAI NEWS----'
     YiCaiCompanyNewsSpider.writeYiCaiCompanyNews()
     
 if __name__ == '__main__':
