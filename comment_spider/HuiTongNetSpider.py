155fba2ebab63f49f6b260030e6902f0706b53f1
unknown <vteamdell0723@.(none)>
2014-05-28 17:43:18 +0800

    SOURCEEDITOR

--- comment_spider/HuiTongNetSpider.py
+++ comment_spider/HuiTongNetSpider.py
@@ -5,13 +5,22 @@ def crawDailyComments(link):
     filterContext = HuiTongNetSpiderUtils.filterContextByTarget(startContext,
                      '<div class="list_content">','<div class="list_flip">')
     print filterContext
-    for i in range(HuiTongNetSpiderUtils.findAllTarget(filterContext,'<div class="list_content01_title">')):
+    #<div class="list_content01
+    i = 0
+    while  i< 8 :
         divideContext =HuiTongNetSpiderUtils.divisionTarget(filterContext,
                                             '<div class="list_content01','<div class="cb"></div>')
         targetContext = divideContext['targetContext']
         filterContext = divideContext['nextContext']
         print '--------------------------------------------------'
+        linkUrl = HuiTongNetSpiderUtils.filterContextByTarget(targetContext,'<a href="','.html"')+'.html'
+        title = HuiTongNetSpiderUtils.filterContextByTarget(targetContext,'target="_blank">','</a>')
+        if i%2 ==0:
+            pubDate = HuiTongNetSpiderUtils.filterContextByTarget(targetContext,'<div class="list_content01_titler">',' </div>')
+        print linkUrl
+        print pubDate
         #print targetContext
+        i += 1
     
     
     
