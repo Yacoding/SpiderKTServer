310dcb73e7ad381cd86912429f60c80e92488a64
unknown <vteamdell0723@.(none)>
2014-06-25 17:47:05 +0800

    SOURCE

--- headlines_spider/FortuneFinanceHLNetSpider.py
+++ headlines_spider/FortuneFinanceHLNetSpider.py
@@ -16,15 +16,32 @@ def crawFinanceHLDataSource(link):
     startContext = FortuneFinanceHLNetSpiderUtils.filterAfterContext(startContext,'</div>')
     pubDate = FortuneFinanceHLNetSpiderUtils.filterContextByTarget(startContext,'>','</font>')
     pubDate = FortuneFinanceHLNetSpiderUtils.removeSpecialCharacter(pubDate)
-    print linkUrl+':'+title+':'+imageUrl+':'+descriptContext+':'+pubDate
-    
-    
+    currentYear = str(time.strftime('%Y',time.localtime(time.time())))
+    pubDate = FortuneFinanceHLNetSpiderUtils.filterAfterContext(pubDate,currentYear)
+    pubDate = currentYear+'-'+pubDate[:2]+'-'+pubDate[2:4]
+    currentList.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'MACRO','FORTUNECHINA'])
     return currentList
      
 def writeFinanceHLDataSource():
     link = 'http://www.fortunechina.com/'
     currentList = crawFinanceHLDataSource(link)
-    
+    conn = FortuneFinanceHLNetSpiderUtils.getMySQLConn()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("DELETE  FROM  HEADLINE_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'FORTUNECHINA'")
+        conn.commit()
+    except conn.Error,e:
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+        conn.rollback()
+    formatSQL = 'INSERT HEADLINE_FINANCENEWS_RESOURCE_TABLE (KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+    try:
+        cursor.executemany(formatSQL,currentList)
+        conn.commit()
+    except conn.Error,e:
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+        conn.rollback()
+    cursor.close()
+    conn.close()
     
     
 if __name__=='__main__':
