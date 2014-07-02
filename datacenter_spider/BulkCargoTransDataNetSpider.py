ab3af87300677624b06a45a88a1b23f9f352d84a
unknown <Administrator@.(none)>
2014-07-02 16:32:56 +0800

    SOURCE

---
+++ datacenter_spider/BulkCargoTransDataNetSpider.py
@@ -0,0 +1,19 @@
+import BulkCargoTransDataNetSpiderUtils
+import uuid
+import time
+
+
+def crawBulkCargoTransDataSource(link):
+    currentList = []
+    
+    
+    return currentList
+
+def writeBulkCargoTransDataSource():
+    link = 'http://app.finance.ifeng.com/data/indu/jgzs.php?symbol=58'
+    
+
+
+
+if __name__=='__main__':
+    writeBulkCargoTransDataSource()
