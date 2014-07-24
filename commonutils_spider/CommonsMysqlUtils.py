4057b3bf1826c363b7c0d1387e11a5d72fafd69f
unknown <zhang.jj@.yougou.com>
2014-07-24 11:26:45 +0800

    SOURCR

--- commonutils_spider/CommonsMysqlUtils.py
+++ commonutils_spider/CommonsMysqlUtils.py
@@ -7,3 +7,11 @@ def returnMySQLConn():
     except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
     return conn
+
+
+def returnErrorMysqlConn():
+    try:
+        conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='4559065',db='crawerror',port=3306,charset='utf8')
+    except MySQLdb.Error,e:
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1]) 
+    return conn   
