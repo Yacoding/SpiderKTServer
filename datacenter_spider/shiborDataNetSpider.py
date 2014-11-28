f884fe3a7e14dd76425517fee9055740b942ba38
wusezhang <wuse_zjj@163.com>
2014-11-28 17:24:37 +0800

    脚本编辑

--- datacenter_spider/shiborDataNetSpider.py
+++ datacenter_spider/shiborDataNetSpider.py
@@ -1,12 +1,21 @@
 import shiborDataNetSpiderUtils
 
 def  crawShiborDataSource(link):
+    currentArray = []
     startContext = shiborDataNetSpiderUtils.returnStartContext(link,'class="infoTitleW">')
     currentTime  = shiborDataNetSpiderUtils.filterContextByTarget(startContext,'class="infoTitleW">','&nbsp;')
     print currentTime
     startContext = shiborDataNetSpiderUtils.filterAfterContext(startContext,'class="shiborquxian"')
     startContext = shiborDataNetSpiderUtils.filterContextByTarget(startContext,'>','<TR>')
     len = shiborDataNetSpiderUtils.findAllTarget(startContext,'<tr>')
+    shiborONValue=0
+    shibor1WValue=0
+    shibor2WValue=0
+    shibor1MValue=0
+    shibor3MValue=0
+    shibor6MValue=0
+    shibor9MValue=0
+    shibor1YValue=0
     for i in range(len):
         targetContext = shiborDataNetSpiderUtils.divisionTarget(startContext,'<tr>','</tr>')
         startContext = targetContext['nextContext']
@@ -14,8 +23,25 @@ def  crawShiborDataSource(link):
         currentContext = shiborDataNetSpiderUtils.removeSpecialCharacter(currentContext)
         shiborValue = shiborDataNetSpiderUtils.filterContextByTarget(currentContext,'<tdwidth="35%"align="center">'
                       ,'</td><tdwidth="10%"align="right">')
-        print  shiborValue
-
+        if i==0:
+            shiborONValue=shiborValue
+        elif i==1:
+            shibor1WValue=shiborValue
+        elif i==2:
+            shibor2WValue=shiborValue
+        elif i==3:
+            shibor1MValue=shiborValue
+        elif i==4:
+            shibor3MValue=shiborValue
+        elif i==5:
+            shibor6MValue =shiborValue
+        elif i==6:
+            shibor9MValue = shiborValue
+        elif i==7:
+            shibor1YValue = shiborValue
+    currentArray.append([currentTime[0:10],shiborONValue,shibor1WValue,shibor2WValue,shibor1MValue,
+                         shibor3MValue,shibor6MValue,shibor9MValue,shibor1YValue])
+    print currentArray
 def writePlateConceptDataSource():
     link = 'http://www.shibor.org/shibor/web/html/shibor.html'
 
