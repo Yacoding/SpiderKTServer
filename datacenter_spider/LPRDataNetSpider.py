3146fd8a21231e1b7aef7ba447e93d545b87fc61
wusezhang <wuse_zjj@163.com>
2014-12-02 17:06:20 +0800

    脚本修改

--- datacenter_spider/LPRDataNetSpider.py
+++ datacenter_spider/LPRDataNetSpider.py
@@ -2,9 +2,25 @@ import LPRDataNetSpiderUtils
 
 
 def crawShiborDataSource(link):
-    print link
-
-
+    startContext = LPRDataNetSpiderUtils.returnStartContext(link,'class="infoTitleW">')
+    currentTime  = LPRDataNetSpiderUtils.filterContextByTarget(startContext,'class="infoTitleW">','&nbsp;')
+    currentTime = currentTime[0:10]
+    startContext = LPRDataNetSpiderUtils.filterAfterContext(startContext,'class="shiborquxian"')
+    startContext = LPRDataNetSpiderUtils.filterAfterContext(startContext,'>')
+    len = LPRDataNetSpiderUtils.findAllTarget(startContext,'<tr>')
+    LPR1YValue = 0
+    for i in range(len):
+        targetContext = LPRDataNetSpiderUtils.divisionTarget(startContext,'<tr>','</tr>')
+        startContext = targetContext['nextContext']
+        currentContext =  targetContext['targetContext']
+        currentContext = LPRDataNetSpiderUtils.removeSpecialCharacter(currentContext)
+        lprValue = LPRDataNetSpiderUtils.filterContextByTarget(currentContext,'<tdwidth="35%"align="center">'
+                      ,'</td><tdwidth="10%"align="right">')
+        if i >0 :
+            break
+        else:
+            LPR1YValue = lprValue
+    print LPR1YValue
 
 def writeShiborConceptDataSource():
     link  = 'http://www.shibor.org/shibor/web/html/LPR.html'
