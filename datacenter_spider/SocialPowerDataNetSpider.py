1870a9fe1e167f3cc17e09298a75baf2ccefe534
wusezhang <wuse_zjj@163.com>
2014-12-05 18:01:53 +0800

    脚本修改

--- datacenter_spider/SocialPowerDataNetSpider.py
+++ datacenter_spider/SocialPowerDataNetSpider.py
@@ -1,15 +1,23 @@
 from  selenium import webdriver
 import SocialPowerDataNetSpiderUtils
+import time
 
 def crawShiborDataSource(link):
     browsor = webdriver.PhantomJS()
     browsor.get(link)
     startContext = browsor.find_element_by_id('datatab').text
-    print  SocialPowerDataNetSpiderUtils.removeSpecialCharacter(startContext)
+    currentYear = time.strftime("%Y",time.localtime())
+    startContext = SocialPowerDataNetSpiderUtils.removeSpecialCharacter(startContext)
+    print  startContext
+    len  =   SocialPowerDataNetSpiderUtils.findAllTarget(startContext,currentYear)
+    for i in range(len):
+        i
+
     browsor.quit()
 
 
 def writeShiborConceptDataSource():
+
     link = 'http://www.shippingdata.cn/free/item.do?lmid=9544F54344034694A5377ED08483A707' \
            '&toplmid=4611C52922C944B5A9325031E6DF4479&type=1'
     crawShiborDataSource(link)
