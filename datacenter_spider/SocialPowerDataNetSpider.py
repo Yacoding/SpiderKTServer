163283445c510b28f1f84119ea05d4e467f8315d
wusezhang <wuse_zjj@163.com>
2014-12-05 16:48:52 +0800

    脚本修改

---
+++ datacenter_spider/SocialPowerDataNetSpider.py
@@ -0,0 +1,19 @@
+import SocialPowerDataNetSpiderUtils
+from  selenium import webdriver
+
+
+def crawShiborDataSource(link):
+    browsor = webdriver.PhantomJS()
+    browsor.get(link)
+    print browsor.find_element_by_id('datatab')
+    browsor.quit()
+
+
+def writeShiborConceptDataSource():
+    link = 'http://www.shippingdata.cn/free/item.do?lmid=9544F54344034694A5377ED08483A707' \
+           '&toplmid=4611C52922C944B5A9325031E6DF4479&type=1'
+    crawShiborDataSource(link)
+
+
+if __name__=='__main__':
+    writeShiborConceptDataSource()
