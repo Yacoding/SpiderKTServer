a501cf9c3296b3ec5dbb3f8d7e0cbbf06335503f
wusezhang <wuse_zjj@163.com>
2014-12-05 10:33:31 +0800

    脚本修改

---
+++ create_table_script/datacenter_lpr_resource_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO `datacenter_lpr_resource_table` VALUES (5.5100, '2014-12-03');
+INSERT INTO `datacenter_lpr_resource_table` VALUES (5.5100, '2014-12-04');
---
+++ create_table_script/datacenter_shibor_resource_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO `datacenter_shibor_resource_table` VALUES ('2014-12-03', 2.6340, 3.4130, 3.9040, 4.1970, 4.1930, 4.3478, 4.4953, 4.6666);
+INSERT INTO `datacenter_shibor_resource_table` VALUES ('2014-12-04', 2.6320, 3.4020, 3.9510, 4.1989, 4.1952, 4.3467, 4.4920, 4.6586);
+INSERT INTO `datacenter_shibor_resource_table` VALUES ('2014-12-05', 2.6280, 3.3900, 3.9580, 4.2180, 4.2177, 4.3460, 4.4885, 4.6487);
