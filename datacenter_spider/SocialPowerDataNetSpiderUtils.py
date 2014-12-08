b0c15d47eeb645eb5b3a00bf26e1d31583a9d6f5
wusezhang <wuse_zjj@163.com>
2014-12-08 17:44:42 +0800

    脚本修改

--- datacenter_spider/SocialPowerDataNetSpiderUtils.py
+++ datacenter_spider/SocialPowerDataNetSpiderUtils.py
@@ -3,21 +3,6 @@ sys.path.append("../commonutils_spider/")
 import CommonsSpiderUtils
 import CommonsMysqlUtils
 
-def filterContextByTarget(context,startfilter,endfilter):
-    return CommonsSpiderUtils.filterContextByTarget(context,startfilter,endfilter)
-
-def findAllTarget(context,filterTarget):
-    return CommonsSpiderUtils.findAllTargets(context,filterTarget)
-
-def removeSpecialCharacter(removeContext):
-    return CommonsSpiderUtils.removeSpecialCharacter(removeContext)
-
-def divisionTarget(startcontext,startfilter,endfilter):
-    return CommonsSpiderUtils.divisionTarget(startcontext,startfilter,endfilter)
-
-def filterAfterContext(startContext,filterContext):
-    return startContext[CommonsSpiderUtils.filterContext(startContext,filterContext)+len(filterContext):]
-
 # GET MYSQL CONNECTION
 def getMySQLConn():
     return CommonsMysqlUtils.returnMySQLConn()
