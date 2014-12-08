14f1ceb4e391f410a1c5dd6a0562179d712030ea
wusezhang <wuse_zjj@163.com>
2014-12-08 18:21:48 +0800

    脚本修改

--- datacenter_spider/PMIDataNetSpider.py
+++ datacenter_spider/PMIDataNetSpider.py
@@ -1,1 +1,25 @@
+from  selenium import webdriver
+import SocialPowerDataNetSpiderUtils
 
+def crawPMIDataSource(link):
+    browsor = webdriver.PhantomJS()
+    browsor.get(link)
+    currentArray = []
+    contextList = browsor.find_element_by_tag_name('table').text.split('\n')
+    contextList = contextList[1:len(contextList)-1]
+    for var in contextList:
+        varList = var.split(' ')
+        varList = varList[1:]
+        for i in range(len(varList)):
+            if varList[i] =='-':
+                varList[i] ='0'
+        currentArray.append(varList)
+    return currentArray
+
+def writePMIDataSource():
+    link = 'http://www.100ppi.com/mac/data---116N.html'
+    currentArray = crawPMIDataSource(link)
+    print currentArray
+
+if __name__=='__main__':
+    writePMIDataSource()
