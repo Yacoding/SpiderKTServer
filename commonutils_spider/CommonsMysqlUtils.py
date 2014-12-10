c1a57a8fc79785fddda53c8dd86adcfe96ac14df
wusezhang <wuse_zjj@163.com>
2014-12-10 17:43:32 +0800

    数据库连接池修改

--- commonutils_spider/CommonsMysqlUtils.py
+++ commonutils_spider/CommonsMysqlUtils.py
@@ -1,10 +1,25 @@
 import MySQLdb
 import DBConfiger
-from DBUtils import PooledDB
+from DBUtils.PooledDB import PooledDB
+
+class DbManager():
+      def __init__(self):
+          self._pool =PooledDB(MySQLdb, user =DBConfiger.getConfig('spiderdatabase','dbuser'),
+                               passwd =DBConfiger.getConfig('spiderdatabase','dbpassword'),
+                               host =DBConfiger.getConfig('spiderdatabase','dbhost'),
+                               port=3306,
+                               db=DBConfiger.getConfig('spiderdatabase','dbname'),
+                               mincached=10,
+                               maxcached=100,
+                               maxshared=50,
+                               maxconnections=10)
+      def getConn(self):
+          self._pool.connection()
+
 
 def returnMySQLConn():
     try:
-        conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='4559065',db='ktproject',port=3306,charset='utf8')
+        conn =DbManager().getConn()
     except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
     return conn
@@ -17,3 +32,5 @@ def returnErrorMysqlConn():
         print "Mysql Error %d: %s" % (e.args[0], e.args[1]) 
     return conn
 
+if __name__=='__main__':
+    returnMySQLConn()
