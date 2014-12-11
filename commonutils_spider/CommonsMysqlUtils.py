9ec9459ec36c972777238b3ebfcf6a8596ddeb9f
wusezhang <wuse_zjj@163.com>
2014-12-11 11:41:10 +0800

    数据库连接池修改

--- commonutils_spider/CommonsMysqlUtils.py
+++ commonutils_spider/CommonsMysqlUtils.py
@@ -24,14 +24,9 @@ def returnMySQLConn():
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
     return conn
 
-
 def returnErrorMysqlConn():
     try:
         conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='4559065',db='crawerror',port=3306,charset='utf8')
     except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1]) 
     return conn
-
-
-if __name__=='__main__':
-    print DbManager('spiderdatabase').getConn()
