4b3568d4e212476dcf140bea055a46890cfff748
wusezhang <wuse_zjj@163.com>
2014-12-11 15:16:16 +0800

    数据库连接池修改

--- commonutils_spider/CommonsMysqlUtils.py
+++ commonutils_spider/CommonsMysqlUtils.py
@@ -13,9 +13,12 @@ class DbManager():
                                maxcached=100,
                                maxshared=50,
                                maxconnections=10)
+
       def getConn(self):
           return self._spiderpool.connection()
 
+##
+_dbManager = DbManager()
 
 def returnMySQLConn():
     try:
@@ -28,11 +31,13 @@ def returnMySQLConn():
 
 
 
-
-
 def returnErrorMysqlConn():
     try:
         conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='4559065',db='crawerror',port=3306,charset='utf8')
     except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1]) 
     return conn
+
+
+if __name__=='__main__':
+    print  returnMySQLConn()
