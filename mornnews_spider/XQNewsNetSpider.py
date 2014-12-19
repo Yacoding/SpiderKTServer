0c1a9bf795d27c5d057b313fa0c17271ce2e821b
wusezhang <wuse_zjj@163.com>
2014-12-19 17:00:40 +0800

    数据库连接池修改

--- mornnews_spider/XQNewsNetSpider.py
+++ mornnews_spider/XQNewsNetSpider.py
@@ -2,6 +2,8 @@ from  selenium import webdriver
 import sys
 sys.path.append("../commonutils_spider/")
 import CommonsMysqlUtils
+import CharacterUtils
+import time
 
 
 def crawXQNewsNetDataSource(link):
@@ -13,7 +15,12 @@ def crawXQNewsNetDataSource(link):
         imageDiv = div.find_element_by_class_name('list_item_pic').find_element_by_tag_name('img')
         imageUrl = imageDiv.get_attribute('src')
         discriptContext = div.find_element_by_class_name('list_item_desc').text
-        print title+imageUrl +discriptContext
+        pubDate = div.find_element_by_class_name('item_time_source') .text
+        pubTime = time.strftime("%m-%d",time.localtime())
+        unicodeText = pubDate[:len(pubDate)-6]
+        print unicodeText
+        if CharacterUtils.is_chinese(unicodeText):
+           print title+imageUrl +discriptContext+pubDate[:len(pubDate)-6]
 
 
 def writeXQNewsNetDataSource():
