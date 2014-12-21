9f71d67fadd475f2e7ef16e321d9e1889e690844
wusezhang <wuse_zjj@163.com>
2014-12-21 11:16:34 +0800

    脚本编辑

--- mornnews_spider/XQNewsNetSpider.py
+++ mornnews_spider/XQNewsNetSpider.py
@@ -2,7 +2,7 @@ from  selenium import webdriver
 import sys
 sys.path.append("../commonutils_spider/")
 import CommonsMysqlUtils
-import CharacterUtils
+import re
 import time
 
 
@@ -18,13 +18,12 @@ def crawXQNewsNetDataSource(link):
         pubDate = div.find_element_by_class_name('item_time_source') .text
         pubTime = time.strftime("%m-%d",time.localtime())
         unicodeText = pubDate[:len(pubDate)-6]
-        print unicodeText
-        if CharacterUtils.is_chinese(unicodeText):
+        if re.match('^[0-9]+$',unicodeText):
            print title+imageUrl +discriptContext+pubDate[:len(pubDate)-6]
 
 
 def writeXQNewsNetDataSource():
-    print  ''
+
 
 
 if __name__=='__main__':
