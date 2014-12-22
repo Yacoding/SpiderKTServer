02ee83be7fff8a78c68b21ded8ee0d0cb2213408
wusezhang <wuse_zjj@163.com>
2014-12-22 10:55:58 +0800

    数据库连接池修改

--- mornnews_spider/XQNewsNetSpider.py
+++ mornnews_spider/XQNewsNetSpider.py
@@ -4,28 +4,36 @@ sys.path.append("../commonutils_spider/")
 import CommonsMysqlUtils
 import re
 import time
+import uuid
 
 
 def crawXQNewsNetDataSource(link):
     browsor = webdriver.PhantomJS()
     browsor.get(link)
     resultList = browsor.find_elements_by_class_name('list_item')
+    listArray = []
     for div in  resultList:
         title = div.find_element_by_class_name('list_item_tit').text
+        linkUrl = div.find_element_by_class_name('list_item_tit').find_element_by_tag_name('a').get_attribute('href')
         imageDiv = div.find_element_by_class_name('list_item_pic').find_element_by_tag_name('img')
         imageUrl = imageDiv.get_attribute('src')
-        discriptContext = div.find_element_by_class_name('list_item_desc').text
-        pubDate = div.find_element_by_class_name('item_time_source') .text
-        pubTime = time.strftime("%m-%d",time.localtime())
-        unicodeText = pubDate[:len(pubDate)-6]
+        descriptContext = div.find_element_by_class_name('list_item_desc').text
+        createDate = div.find_element_by_class_name('item_time_source') .text
+        pubDate = time.strftime("%Y-%m-%d %X",time.localtime())
+        unicodeText = createDate[:len(pubDate)-6]
         if not re.match('^[0-9]',unicodeText):
-           print title+imageUrl +discriptContext+pubDate[:len(pubDate)-6]
-
+           listArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'STOCK','XQNET'])
+    return listArray
 
 def writeXQNewsNetDataSource():
-    print ''
-
-
-if __name__=='__main__':
     link = 'http://xueqiu.com/today/us'
-    crawXQNewsNetDataSource(link)
+    dbManager = CommonsMysqlUtils._dbManager
+    SQL = " DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE " \
+          " WHERE  SOURCEFLAG = 'XQNET' AND  NEWSFLAG='STOCK'"
+    dbManager.executeUpdateOrDelete(SQL)
+
+    currentArray = crawXQNewsNetDataSource(link)
+    formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE ' \
+                '(KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG) ' \
+                ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+    dbManager.executeManyInsert(formatSQL,currentArray)
--- mornnews_spider/YiCaiStockNetSpider.py
+++ mornnews_spider/YiCaiStockNetSpider.py
@@ -35,12 +35,15 @@ def writeYiCaiStockDataSource():
     conn = YiCaiStockNetSpiderUtils.getMySQLConn()
     cursor = conn.cursor()
     try:
-        cursor.execute("DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'YICAINET' AND  NEWSFLAG='STOCK'")
+        cursor.execute("DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  "
+                       " WHERE  SOURCEFLAG = 'YICAINET' AND  NEWSFLAG='STOCK'")
         conn.commit()
     except conn.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
         conn.rollback()
-    formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE (KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+    formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE ' \
+                '(KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG) ' \
+                ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
     try:
         cursor.executemany(formatSQL,currentList)
         conn.commit()
