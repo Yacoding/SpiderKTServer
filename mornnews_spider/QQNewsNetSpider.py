25238a4b521b17fef76bfebcf683d5ec805a9446
wusezhang <wuse_zjj@163.com>
2014-12-30 15:27:15 +0800

    数据库连接池修改

--- mornnews_spider/QQNewsNetSpider.py
+++ mornnews_spider/QQNewsNetSpider.py
@@ -1,47 +1,42 @@
-import QQNewsNetSpiderUtils
-import uuid
+from  selenium import webdriver
+import sys
+sys.path.append("../commonutils_spider/")
+import CommonsMysqlUtils
+import re
 import time
+import uuid
 
 
-def crawMorningDailyNews(linkUrl,WebNet):
-    currentList = []
-    startContext = QQNewsNetSpiderUtils.returnStartContext(linkUrl,'<ul class="jdbgList">')
-    startContext = QQNewsNetSpiderUtils.filterContextByTarget(startContext,'<ul class="jdbgList">','</ul>')
-    len = QQNewsNetSpiderUtils.findAllTarget(startContext,'<li>')
-    for i in range(len):
-        targetContext = QQNewsNetSpiderUtils.divisionTarget(startContext,'<li>','</li>')
-        startContext = targetContext['nextContext']
-        currentContext =  targetContext['targetContext']
-        currentContext = QQNewsNetSpiderUtils.removeSpecialCharacter(currentContext)
-        imageUrl = QQNewsNetSpiderUtils.filterContextByTarget(currentContext,'src="','"alt=')
-        linkUrl = QQNewsNetSpiderUtils.filterContextByTarget(currentContext,'href="','"><imgwidth')
-        title = QQNewsNetSpiderUtils.filterContextByTarget(currentContext,'alt="','"></a><divclass')
-        descriptContext = QQNewsNetSpiderUtils.filterAfterContext(currentContext, '<divclass="text">')
-        descriptContext = QQNewsNetSpiderUtils.filterContextByTarget(descriptContext,'','<atarget')
-        pubDate = QQNewsNetSpiderUtils.filterContextByTarget(currentContext,'<spanclass="aTime">','</span><spanclass="techTag">')
-        currentList.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'CHINA','QQNET'])
-    return currentList
+def crawMorningQQDailyNews(link):
+    browsor = webdriver.PhantomJS()
+    browsor.get(link)
+    resultList = browsor.find_element_by_class_name('jdbgList').find_elements_by_tag_name('li')
+    listArray = []
+    for div in  resultList:
+         descriptContext =  div.find_element_by_class_name('text').text
+         linkUrl =  div.find_element_by_class_name('pic').get_attribute('href')
+         imageUrl = div.find_element_by_tag_name('img').get_attribute('src')
+         title  = div.find_element_by_class_name('tit').text
+         createDate  = div.find_element_by_class_name('aTime').text
+         pubDate = time.strftime("%Y-%m-%d",time.localtime())
+         if createDate == pubDate:
+           listArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'CHINA','QQNET'])
+    return listArray
     
-def writeMorningDailyNews():
+def writeMorningQQDailyNews():
     link = 'http://finance.qq.com/focus.htm'
-    webNet = ''
-    currentList =  crawMorningDailyNews(link,webNet)
-    conn = QQNewsNetSpiderUtils.getMySQLConn()
-    cursor = conn.cursor()
-    try:
-        cursor.execute("DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'QQNET'")
-        conn.commit()
-    except conn.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-    formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE ' \
+
+    dbManager = CommonsMysqlUtils._dbManager
+    SQL = " DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'QQNET' " \
+          " AND  NEWSFLAG='STOCK'"
+    dbManager.executeUpdateOrDelete(SQL)
+
+    currentArray = crawMorningQQDailyNews(link)
+    formatSQL =  'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE ' \
                 '(KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG)' \
                 ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
-    try:
-        cursor.executemany(formatSQL,currentList)
-        conn.commit()
-    except conn.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-    cursor.close()
-    conn.close()
+    dbManager.executeManyInsert(formatSQL,currentArray)
+
+
+if __name__=='__main__':
+    writeMorningQQDailyNews()
