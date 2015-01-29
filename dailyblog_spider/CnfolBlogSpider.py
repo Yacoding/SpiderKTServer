5b19d9c6db6a872458811a06c57e00c293e059c3
wusezhang <wuse_zjj@163.com>
2015-01-29 10:41:56 +0800

    数据库连接池修改

--- dailyblog_spider/CnfolBlogSpider.py
+++ dailyblog_spider/CnfolBlogSpider.py
@@ -1,11 +1,26 @@
-import CnfolBlogSpiderUtils 
+from  selenium import webdriver
+import sys
+sys.path.append("../commonutils_spider/")
+import CommonsMysqlUtils
+import time
+import uuid
 
 
 
-def dailyCnfolBlogSpider(linkUrl):
-    startContext =  CnfolBlogSpiderUtils.returnStartContext(linkUrl, '<div class="articleListAll">')            
-    print startContext
-
+def dailyCnfolBlogSpider(link):
+    'articleListAll'
+    browsor = webdriver.PhantomJS()
+    browsor.get(link)
+    resultList = browsor.find_elements_by_class_name('ArticleBox')
+    listArray = []
+    for div in  resultList:
+        descriptContext = div.find_element_by_class_name('strContent').text
+        titleHtml = div.find_element_by_tag_name('h2').find_element_by_tag_name('a')
+        title = titleHtml.text
+        linkUrl = titleHtml.get_attribute('href')
+        pubtime = div.find_element_by_class_name('Time').text
+        pubtime = pubtime.encode('gbk')
+        print pubtime[0]+pubtime[2]
 
 def writeDailyCnfolBlogSpider():
     linkUrl = 'http://new.blog.cnfol.com/zhangping626'
