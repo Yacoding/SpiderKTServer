1cda7b6afc9efeb4616ce0971a6252b868a4691d
zhang.jj <wuse_zjj@163.com>
2015-02-03 18:27:23 +0800

    数据库连接池修改

--- mornnews_spider/YiCaiFinanceNetSpider.py
+++ mornnews_spider/YiCaiFinanceNetSpider.py
@@ -16,20 +16,22 @@ def crawYCFinanceHLDataSource(link):
          try:
               imageObj = currentDiv.find_element_by_tag_name('img')
               imageUrl = imageObj.get_attribute('src')
+
               titleObj = currentDiv.find_element_by_tag_name('hi')
               title = titleObj.text
+              print  title
               linkUrl = titleObj.find_element_by_tag_name('a').get_attribute('href')
-              descriptContext = currentDiv.find_element_by_tag_name('p').text
+
               pubDate = time.strftime("%Y-%m-%d %X",time.localtime())
-              listArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'CHINA','YCNET'])
-              print descriptContext
+              listArray.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,'','CHINA','YCNET'])
+              print linkUrl
          except NoSuchElementException,e:
               continue
+    print listArray
     return listArray
 
 def writeFinanceHLDataSource():
     link = 'http://www.yicai.com/finance/'
-    currentList = crawYCFinanceHLDataSource(link)
 
     dbManager = CommonsMysqlUtils._dbManager
     SQL = " DELETE  FROM  MORNING_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'YCNET' " \
@@ -37,7 +39,7 @@ def writeFinanceHLDataSource():
     dbManager.executeUpdateOrDelete(SQL)
 
     currentArray = crawYCFinanceHLDataSource(link)
-    formatSQL =  'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE ' \
+    formatSQL = 'INSERT MORNING_FINANCENEWS_RESOURCE_TABLE ' \
                 '(KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG)' \
                 ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
     dbManager.executeManyInsert(formatSQL,currentArray)
