7efb2a648dc42098e1e12d4dc398f5c65256cdee
zhang.jj <wuse_zjj@163.com>
2015-03-02 14:28:11 +0800

    资源变更

--- headlines_spider/ForbesFinanceHLNetSpider.py
+++ headlines_spider/ForbesFinanceHLNetSpider.py
@@ -1,43 +1,33 @@
-import ForbesFinanceHLNetSpiderUtils
 import time
 import uuid
+import sys
+sys.path.append("../commonutils_spider/")
+import CommonsMysqlUtils
+from  selenium import webdriver
 
-def crawFinanceHLDataSource(link,webNet):
+
+def crawFinanceHLDataSource(link):
     currentList = []
-    startContext =  ForbesFinanceHLNetSpiderUtils.returnStartContext(link,'<div class="div_news">')
-    startContext =  ForbesFinanceHLNetSpiderUtils.filterContextByTarget(startContext,'<div class="div_news">','<div class="side_news">') 
-    startContext =  ForbesFinanceHLNetSpiderUtils.removeSpecialCharacter(startContext)
-    startContext =  ForbesFinanceHLNetSpiderUtils.filterAfterContext(startContext,'src="')
-    imageUrl  = ForbesFinanceHLNetSpiderUtils.filterContextByTarget(startContext,'','"/>')
-    startContext =  ForbesFinanceHLNetSpiderUtils.filterAfterContext(startContext,'href="')
-    linkUrl = webNet+ForbesFinanceHLNetSpiderUtils.filterContextByTarget(startContext,'','">')
-    title = ForbesFinanceHLNetSpiderUtils.filterContextByTarget(startContext,'>','</a>')
-    startContext =  ForbesFinanceHLNetSpiderUtils.filterAfterContext(startContext,'</a>')
-    descriptContext = ForbesFinanceHLNetSpiderUtils.filterContextByTarget(startContext,'>','</a>')
-    descriptContext = ForbesFinanceHLNetSpiderUtils.removeSpecialCharacter(descriptContext)
-    startContext =  ForbesFinanceHLNetSpiderUtils.filterAfterContext(startContext,'<divclass="news_link3">')
-    pubDate = ForbesFinanceHLNetSpiderUtils.filterContextByTarget(startContext,'','</div>')
+    browsor = webdriver.PhantomJS()
+    browsor.get(link)
+    currentDiv  = browsor.find_element_by_class_name('div_news')
+    title = currentDiv.find_element_by_class_name('news_title').text
+    linkUrl = currentDiv.find_element_by_class_name('news_title').get_attribute('href')
+    descriptContext = currentDiv.find_element_by_class_name('news_detail').text
+    pubDate = currentDiv.find_element_by_class_name('news_link3').text
+    imageUrl =  currentDiv.find_element_by_tag_name('img').get_attribute('src')
     currentList.append([str(uuid.uuid1()),linkUrl,imageUrl,title,pubDate,descriptContext,'MACRO','FORBESCHINA'])
     return currentList
      
 def writeFinanceHLDataSource():
     link = 'http://www.forbeschina.com/investment/macrography/'
-    webNet = 'http://www.forbeschina.com'
-    currentList = crawFinanceHLDataSource(link,webNet)
-    conn = ForbesFinanceHLNetSpiderUtils.getMySQLConn()
-    cursor = conn.cursor()
-    try:
-        cursor.execute("DELETE  FROM  HEADLINE_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'FORBESCHINA'")
-        conn.commit()
-    except conn.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-    formatSQL = 'INSERT HEADLINE_FINANCENEWS_RESOURCE_TABLE (KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
-    try:
-        cursor.executemany(formatSQL,currentList)
-        conn.commit()
-    except conn.Error,e:
-        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-        conn.rollback()
-    cursor.close()
-    conn.close()
+    currentList = crawFinanceHLDataSource(link)
+
+    dbManager = CommonsMysqlUtils._dbManager
+    SQL = "DELETE  FROM  HEADLINE_FINANCENEWS_RESOURCE_TABLE  WHERE  SOURCEFLAG = 'FORBESCHINA'"
+    dbManager.executeUpdateOrDelete(SQL)
+
+    formatSQL = ' INSERT HEADLINE_FINANCENEWS_RESOURCE_TABLE' \
+                ' (KEYID,LINKURL,IMAGEURL,TITLE,PUBDATE,DESCRIPTCONTEXT,NEWSFLAG,SOURCEFLAG)' \
+                ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+    dbManager.executeManyInsert(formatSQL,currentList)
